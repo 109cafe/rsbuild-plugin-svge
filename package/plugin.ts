48e14fdaf148e96d14bffb5a9a4202ecda130a05
xc2 <xc2@users.noreply.github.com>
2024-05-09 00:41:16 +0800

    chore: jsr initial

---
+++ package/plugin.ts
@@ -0,0 +1,62 @@
+import type { RsbuildPlugin, RuleSetRule } from "@rsbuild/shared";
+import type { Config } from "@svgr/core";
+
+export interface MassSvgRule {
+  test: NonNullable<RuleSetRule["test"]>;
+  svgDefaultExport: "component" | "url";
+  issuer?: NonNullable<RuleSetRule["issuer"]>;
+  options?: Config;
+}
+
+export interface PluginMassSvgOptions {
+  rules: MassSvgRule[];
+}
+
+export function pluginMassSvg(options: PluginMassSvgOptions): RsbuildPlugin {
+  return {
+    name: "rsbuild-plugin-mass-svg",
+    pre: ["rsbuild:svgr"],
+    setup(api) {
+      api.modifyBundlerChain((chain, { CHAIN_ID }) => {
+        if (options.rules.length) {
+          const rule = chain.module.rule(CHAIN_ID.RULE.SVG);
+          const firstOneOf = rule.oneOfs.values()[0];
+
+          for (const [index, config] of Object.entries(options.rules)) {
+            const current = rule.oneOf(`mass-svgr-${index}`);
+            current.test(config.test).before((firstOneOf as any).ruleName);
+            if (config.issuer) {
+              current.issuer(config.issuer);
+            }
+            if (config.svgDefaultExport === "url") {
+              current.merge(rule.oneOf(CHAIN_ID.ONE_OF.SVG_ASSET).entries()).test(config.test);
+            } else if (config.svgDefaultExport === "component") {
+              const use = current.use("svgr");
+
+              use.loader(require.resolve("./svgr-loader"));
+
+              use.options({ exportType: "default", ...config.options });
+
+              const jsRule = chain.module.rules.get(CHAIN_ID.RULE.JS);
+
+              for (const jsUseId of [CHAIN_ID.USE.SWC, CHAIN_ID.USE.BABEL]) {
+                const jsUse = jsRule.uses.get(jsUseId);
+
+                if (!jsUse) {
+                  continue;
+                }
+
+                current
+                  .use(jsUseId)
+                  .before("svgr")
+                  .loader(jsUse.get("loader"))
+                  .options(jsUse.get("options"));
+                break;
+              }
+            }
+          }
+        }
+      });
+    },
+  };
+}
